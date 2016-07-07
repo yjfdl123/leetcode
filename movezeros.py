ca7200a51deb72931407d8dff0c60c1ea1e40f50
yjfdl123 <yjfdl123@163.com>
2016-07-08 00:41:49 +0800

    move zeros

---
+++ movezeros.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        start = 0
+        end = 0
+        maxlen = len(nums)
+        while start < maxlen and nums[start] != 0:
+            start = start + 1
+        end = start + 1
+        print start
+        while end < maxlen:
+            nums[start], nums[end] = nums[end], nums[start]
+            while nums[start] != 0:
+                start += 1
+            end = end + 1
+            while end < maxlen and nums[end] == 0:
+                end += 1
+        print end
+        print nums
+
+
+if __name__ == '__main__':
+    test = Solution()
+    nums = [1]
+    test.moveZeroes(nums)
