3be39003bc9410edb1f969bb91d13b5686621da8
yjfdl123 <yjfdl123@163.com>
2016-10-08 23:43:04 +0800

    蛙跳游戏

--- middle/055jump_game.py
+++ middle/055jump_game.py
@@ -6,14 +6,36 @@ class Solution(object):
         """
         boolst = [False for num in nums]
         boolst[0] = True
-        for idx in range(len(nums)):
-        	if boolst[idx]:
-        		for step in range(1,nums[idx]+1):
-        			if idx+step<len(nums) and not boolst[idx+step]:
-        				boolst[idx+step] = True
-        return boolst[len(nums)-1]
+        start,end = 0,len(nums)
+        far = 0
+        while start<end :
+            if start<=far:
+                if start+nums[start]>far : far=start+nums[start]
+                if far>=end-1:return True
+                start+=1
+            else:
+                return False
+        return far>=end-1
+
+
+
+
+
 
 
 so = Solution()
 print so.canJump([2,3,1,1,4])
 print so.canJump([3,2,1,0,4])
+
+
+
+
+
+        # boolst = [False for num in nums]
+        # boolst[0] = True
+        # for idx in range(len(nums)):
+        #   if boolst[idx]:
+        #       for step in range(1,nums[idx]+1):
+        #           if idx+step<len(nums) and not boolst[idx+step]:
+        #               boolst[idx+step] = True
+        # return boolst[len(nums)-1]
