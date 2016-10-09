3a9ac2e15eace7e9d4b336861067ce5d066fa620
杨江峰 <yjfn2141@corp.netease.com>
2016-10-09 21:27:44 +0800

    请找出第n个丑数

--- middle/264ugly_number2.py
+++ middle/264ugly_number2.py
@@ -4,22 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n<1: return 0
-        ugly=[1,2,3]
-        while len(ugly)<n:
-            mini= min([self.find_next_ugly(ugly,2), self.find_next_ugly(ugly,3), self.find_next_ugly(ugly,5)])
-            ugly.append( mini )
-        return ugly[-1]
+        ret=[1]
+        count2,count3,count5=0,0,0
+        while len(ret)<n:
+            mini = min(ret[count2]*2,ret[count3]*3,ret[count5]*5)
+            if ret[count2]*2==mini:count2+=1
+            if ret[count3]*3==mini:count3+=1
+            if ret[count5]*5==mini:count5+=1
+            ret.append(mini)
+        return ret[-1]
 
-    def find_next_ugly(self,ugly,base):
-        start,end = 0,len(ugly)-1
-        while start<=end:
-            mid = (start+end)/2
-            if (ugly[mid]*base>ugly[-1]) and (ugly[mid-1]*base<ugly[-1]):
-                return ugly[mid]*base
-            elif ugly[mid]*base<ugly[-1]:
-                start=mid+1
-            else: 
-                end=mid-1
+
+
 so=Solution()
-print so.nthUglyNumber(372)
+print so.nthUglyNumber(10)
