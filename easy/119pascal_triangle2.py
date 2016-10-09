c0d7a4e168a498363007b086cdf0b821fb62250c
yjfdl123 <yjfdl123@163.com>
2016-10-10 00:24:03 +0800

    杨辉三角形的第k行

--- easy/119pascal_triangle2.py
+++ easy/119pascal_triangle2.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        prev = [1 for idx in range(rowIndex+1)]
-        import copy
-        cur = copy.deepcopy(prev)
+        prev=[1]
+        row=1
+        while row<=rowIndex:
+        	nex=[0]*(len(prev)+1)
+        	nex[0],nex[-1]=prev[0],prev[-1]
+        	for index in range(1,len(nex)-1):
+        		nex[index]=prev[index-1]+prev[index]
+        	prev,nex=nex,[]
+        	row+=1
+        return prev
+
+
         
 
 so =Solution()
-print so.getRow(4)
+print so.getRow(3)
