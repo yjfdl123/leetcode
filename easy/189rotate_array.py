abfb7151c994a179ea063739b3ad76aae070ad4d
杨江峰 <yjfn2141@corp.netease.com>
2016-10-10 12:40:03 +0800

    旋转数组

--- easy/189rotate_array.py
+++ easy/189rotate_array.py
@@ -5,5 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        length=len(nums)
+        k=k%length
+        k=length-k
         if k>0:
+        	nums.extend(nums[:k])
+        	for idx in range(k,len(nums)):
+        		nums[idx-k]=nums[idx]
+        	for idx in range(k):
+        		nums.pop()
+        return nums
+so=Solution()
+print so.rotate([1,2,3,4,5],4)
+
+
         	
