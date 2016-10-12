1bab1dac4f7420f79a985110a604f7a267045bce
杨江峰 <yjfn2141@corp.netease.com>
2016-10-12 19:16:05 +0800

    059打印1-n的螺旋矩阵

--- middle/059spiral_matrix2.py
+++ middle/059spiral_matrix2.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 class Solution(object):
     def generateMatrix(self, n):
         """
@@ -5,29 +6,42 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         if n<=0:return []
-        if n==1: return [1]
-        return self.loop(0,n,0,n,1,n*n)
+        if n==1: return [[1]]
+        matrix = [[0 for y in range(n)] for x in range(n)]
+        self.loop(matrix,0,n-1,0,n-1,1,n*n)
+        return matrix
 
-    def loop(self,hstart,hend,wstart,wend,count,n2):
-    	ret = []
-    	if hend<hstart or wend<wstart: return ret
-    	#第一行
-    	tmp=wstart
-    	while tmp<=wend:
-    		
-    	ret.extend( matrix[hstart][wstart:wend+1] )
-    	if hend>hstart:
-    		#最后一列
-    		for h in range(hstart+1,hend+1):
-    			print h,wend
-    			ret.append(matrix[h][wend])
-    		if wend>wstart:
-    			#最后一行
-    			for w in range(wend-1,wstart-1,-1):
-    				ret.append(matrix[hend][w])
-    			#第一列
-    			for h in range(hend-1,hstart,-1):
-    				ret.append( matrix[h][wstart])
-    	ret.extend( self.loop(matrix,hstart+1,hend-1,wstart+1,wend-1))
-    	return ret        
+    def loop(self,matrix, hstart,hend,wstart,wend,count,n2):
+        if hstart<=hend and wstart<=wend:
+            #第一行
+            tmp=wstart
+            while tmp<=wend:
+                matrix[hstart][tmp]=count
+                tmp+=1
+                count+=1
+            # ret.extend( matrix[hstart][wstart:wend+1] )
+            if hend>hstart:
+                #最后一列
+                for h in range(hstart+1,hend+1):
+                    # print h,wend
+                    matrix[h][wend]=count
+                    count+=1
+                    # ret.append(matrix[h][wend])
+                if wend>wstart:
+                    #最后一行
+                    for w in range(wend-1,wstart-1,-1):
+                        matrix[hend][w]=count
+                        count+=1
+                        # ret.append(matrix[hend][w])
+                    #第一列
+                    for h in range(hend-1,hstart,-1):
+                        matrix[h][wstart]=count
+                        count+=1
+                        # ret.append( matrix[h][wstart])
+            self.loop(matrix,hstart+1,hend-1,wstart+1,wend-1,count,n2)
+            # ret.extend( self.loop(matrix,hstart+1,hend-1,wstart+1,wend-1))
+            # return ret        
+so=Solution()
+print so.generateMatrix(10)
+
 
