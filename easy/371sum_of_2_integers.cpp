3832a84ed1692fe55912e204d2f95c6b81dc2ceb
yangjiangfeng <yjfdl123@163.com>
2016-10-17 12:46:49 +0800

    371两个数之和

---
+++ easy/371sum_of_2_integers.cpp
@@ -0,0 +1,15 @@
+#include "cpphelp.cpp"
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int carry=0;
+        while (b!=0){
+            carry = (a&b)<<1;
+            a = a^b;
+            b=carry;
+        };
+        return a;
+    }
+};
+int main(){
+};
